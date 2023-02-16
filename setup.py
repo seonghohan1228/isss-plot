23d73d007c4a883406628a18021a301aea7a1cbe
Seongho Han <seonghohan1228@kaist.ac.kr>
2023-02-16 22:17:14 +0900

    write setup.py file

--- setup.py
+++ setup.py
@@ -2,10 +2,9 @@ from setuptools import setup
 
 setup(
     name='isss-plot',
-    version='1.0.0',
+    version='0.0.1',
     description='This package plots data from hdf files.',
     author='Seongho Han',
     author_email='seonghohan1228@kaist.ac.kr'
     url='https://github.com/seonghohan1228/isss-plot',
-    install_requires=[]
 )
