3ad3a80a523721d1f3fbfbac4972d3ff24291736
Seongho Han <seonghohan1228@kaist.ac.kr>
2023-02-17 13:39:10 +0900

    removed unused files and directories

---
+++ main.py
@@ -0,0 +1,17 @@
+# main.py
+
+# import h5py
+
+def main(filename):
+    # Load file
+    # with h5py.File(filename)
+    print(filename)
+    # Read and process data
+    
+    
+    # Plot
+
+if __name__ == "__main__":
+    main(filename)
+
+
