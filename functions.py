bf149659135ceeccdf9cd7dd145e2f1b0fbd22ee
Seongho Han <seonghohan1228@kaist.ac.kr>
2023-02-19 19:21:05 +0900

    Plot of HEPD, MEPD PC1 data

--- functions.py
+++ functions.py
@@ -2,8 +2,10 @@ import os
 import h5py
 import numpy as np
 import pandas as pd
-from constants import *
+import matplotlib.pyplot as plt
 
+MEPD_DT_A = 3
+MEPD_DT_B = 4
 
 # Finding filepaths with corresponding orbit number
 def find_filepath(orbit_no, data_folder, keyword1, keyword2):
@@ -34,44 +36,99 @@ def find_filepath(orbit_no, data_folder, keyword1, keyword2):
 
 
 # Printing group structure of HDF file that has 2 layers of groups
-def tree(group1, group2):
+def tree(groups, dataset):
     print('File structure')
-    for g1 in group1:
-        print(f' -- {g1}')
-        for g2 in group2:
-            print(f'   -- {g2}')
+    for g in groups:
+        print(f' -- {g}')
+        for ds in dataset:
+            print(f'   -- {ds}')
 
 
 # Reading hdf file data
-def read_hdf(orbit_no, data_folder, show_tree):
+def read_hdf(orbit_no, data_folder='./isss_data', show_tree=False):
     filepaths = find_filepath(orbit_no, data_folder, 'HEPD', 'MEPD')
     hepd_data, mepd_data = [], []
     for fp in filepaths:
         with h5py.File(fp) as f:
             print(f'Opening {fp}')
             # Storing groups
-            group1 = np.array(list(f.keys()))
-            group2 = np.array([])
-            for group in group1:
-                group2 = np.append(group2, list(f.get(group)))
+            groups = np.array(list(f.keys()))
+            datasets = np.array([])
+            for group in groups:
+                datasets = np.append(datasets, list(f.get(group)))
             # Show file structure
             if show_tree == True:
-                tree(group1, group2)
-            data_index = np.array(f.get(f'{group1[0]}/{group2[0]}'))
+                tree(groups, datasets)
+            data_index = np.array(f.get(f'{groups[0]}/{datasets[0]}'))
             # Read and store data as a pandas dataframe
             if 'HEPD' in fp:
                 for i in range(2, 4):
-                    df_col = np.array(f.get(f'{group1[0]}/{group2[2*i]}'))
-                    df_data = np.array(f.get(f'{group1[0]}/{group2[2*i+1]}'))
+                    df_col = np.array(f.get(f'{groups[0]}/{datasets[2*i]}'))
+                    df_data = np.array(f.get(f'{groups[0]}/{datasets[2*i+1]}'))
                     hepd_data.append(pd.DataFrame(df_data, 
                                     index=range(len(df_data)), columns=df_col))
             elif 'MEPD' in fp:
                 for i in range(2, 4):
-                    df_col = np.array(f.get(f'{group1[0]}/{group2[2*i]}'))
-                    df_data = np.array(f.get(f'{group1[0]}/{group2[2*i+1]}'))
+                    df_col = np.array(f.get(f'{groups[0]}/{datasets[2*i]}'))
+                    df_data = np.array(f.get(f'{groups[0]}/{datasets[2*i+1]}'))
                     mepd_data.append(pd.DataFrame(df_data, 
                                     index=range(len(df_data)), columns=df_col))
         print(f'Closed {fp}')
     return hepd_data, mepd_data
+
+
+def find_data(data, dataset_no, index):
+    return data[dataset_no].loc[:, bytes(index, 'utf-8')]
+
+
+def graph_plot(hepd_data, mepd_data, orbit_no, plot_folder='./plot', 
+            plot_name='plot.png'):
+    hepd_time = find_data(hepd_data, 0, 'TIME_x')
+    mepd_time = find_data(mepd_data, 0, 'TIME')
+    hepd_pc1 = find_data(hepd_data, 0, 'PC1')
+    mepd_pc1 = find_data(mepd_data, 0, 'PC1')
+    mepd_dt = find_data(mepd_data, 0, 'DT')
+    mepd_time_a, mepd_time_b, mepd_pc1_a, mepd_pc1_b = [], [], [], []
+    # Find where DT in MEPD_SCI data changes to divide MEPD-A and MEPD-B
+    for i in range(len(mepd_dt)):
+        if mepd_dt[i] == MEPD_DT_A:
+            mepd_time_a.append(mepd_time[i])
+            mepd_pc1_a.append(mepd_pc1[i])
+        elif mepd_dt[i] == MEPD_DT_B:
+            mepd_time_b.append(mepd_time[i])
+            mepd_pc1_b.append(mepd_pc1[i])
+
+    fig = plt.figure(constrained_layout=True, figsize=(12, 15))
+    fig.suptitle(f'Orbit: {orbit_no} Date: ')
+    subfigs = fig.subfigures(1, 2, wspace=0.05)
+    subfigsnest = subfigs[0].subfigures(3, 1, height_ratios=[2, 3, 5])
+    axesnest = subfigsnest[0].subplots(1, 2)
     
+    axesnest[0].plot(hepd_pc1, hepd_time)
+    axesnest[1].plot(mepd_pc1_a, mepd_time_a, color='black', marker='x', 
+                    markersize=0.1, linestyle='-')
+    axesnest[1].plot(mepd_pc1_b, mepd_time_b, color='red', marker='D', 
+                    markersize=0.1, linestyle='--')
+    
+    '''
+    for ax in axesnest:
+        pc = plt.example_plot(ax)
+        '''
+    '''
+    fig.set_figwidth(10)
+    fig.set_figheight(30)
+    gs0 = plt.GridSpec(1, 2, figure=fig)
+    gs00 = gs0[0].subgridspec(11, 1)
+    gs01 = gs0[1].subgridspec(11, 1)
+    ax1 = fig.add_subplot(gs00[:2, :])
+    ax2 = fig.add_subplot(gs00[2:5, :])
+    ax3 = fig.add_subplot(gs00[5:8, :])
+    ax4 = fig.add_subplot(gs00[8:11, :])
+    ax5 = fig.add_subplot(gs01[:4, :])
+    ax6 = fig.add_subplot(gs01[4:8, :])
+    ax7 = fig.add_subplot(gs01[8:11, :])
+    '''
+   
+    plt.savefig(f'{plot_folder}/{plot_name}', dpi=200)
+    print(f'Saved plot to {plot_folder}/{plot_name}')
 
--- main.py
+++ main.py
@@ -1,11 +1,11 @@
-from constants import *
 from functions import *
 
 ORB_NO = 8737
 
 def main():
-    hepd_data, mepd_data = read_hdf(ORB_NO, data_folder='isss_data', 
-                                    show_tree=False)
+    hepd_data, mepd_data = read_hdf(ORB_NO)
+    graph_plot(hepd_data, mepd_data, ORB_NO, plot_folder='plot', 
+            plot_name='plot.png')
 
 if __name__ == "__main__":
     main()
